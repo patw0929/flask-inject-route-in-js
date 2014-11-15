f2388a1b96172c5110b63c25e481ece88159ca2d
patw <patw.hi@gmail.com>
2014-11-15 22:58:14 +0800

    Add API method named 'test'
    return JSON object via POST method

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/test', methods=['POST'])
+def test():
+    return jsonify({"result": "it works!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=int(9527))
