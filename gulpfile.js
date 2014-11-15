dee8006548f91e6b5e11ce5aafe3765aef8ed55b
patw <patw.hi@gmail.com>
2014-11-15 23:24:43 +0800

    Add gulp replacing handler

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var gulp = require('gulp'),
 
 gulp.task('js', function () {
   return gulp.src("assets/js/*.js")
+             .pipe(replace({
+              patterns: [
+                {
+                  json: {
+                    "test": "/say"
+                  }
+                }
+              ]
+             }))
              .pipe(gulp.dest("static/js"));
 });
 
