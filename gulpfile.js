d8692f89a6a7fe763500c3092bf820c0e1ff14d8
patw <patw.hi@gmail.com>
2014-11-15 23:36:28 +0800

    Accroding routes.json content to replace route paths

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,26 @@ var gulp = require('gulp'),
     replace = require('gulp-replace-task');
 
 gulp.task('js', function () {
+  var fs = require('fs'),
+      routes_file = './routes.json',
+      routes_json;
+
+  try {
+    routes_json = require(routes_file);
+  } catch (error) {
+    if (error.code === 'MODULE_NOT_FOUND') {
+      console.error('Please run "fab generate_routes_json" first.');
+      process.nextTick(function () {
+        process.exit(0);
+      });
+    }
+  }
+
   return gulp.src("assets/js/*.js")
              .pipe(replace({
               patterns: [
                 {
-                  json: {
-                    "test": "/say"
-                  }
+                  json: routes_json
                 }
               ]
              }))
